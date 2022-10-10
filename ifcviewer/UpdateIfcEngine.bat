4eeb5187ac0bdf7bd94e669b04174eb88e86ca30
Igor.Sokolov <I.Sokolov@ya.ru>
2022-10-10 07:37:31 +0400

    update ifcEngine.dll

--- ifcviewer/UpdateIfcEngine.bat
+++ ifcviewer/UpdateIfcEngine.bat
@@ -3,16 +3,14 @@ echo Check IFC engine includes
 if .%RDF_IFC_ENGINE_INCLUDE%.==.. goto IncUpdated
 echo echo Update IFC engine includes 
 xcopy %RDF_IFC_ENGINE_INCLUDE% IfcEngine\include /F /Y
-copy %RDF_IFC_ENGINE_INCLUDE%..\ToolBox\ModelChecker.h IfcEngine\include /Y
+xcopy %RDF_IFC_ENGINE_INCLUDE%..\ToolBox\ModelChecker.h IfcEngine\include /F /Y
 :IncUpdated
 
 echo Check IFC engine binaries
 if .%RDF_IFC_ENGINE_LIB%.==.. goto LibUpdated
-echo Update ifcengine\lib\x64\ifcengine.lib
-copy %RDF_IFC_ENGINE_LIB%Debug\ifcengine.lib ifcengine\lib\x64\ifcengine.lib /Y
-echo Update ifcengine\lib\x64\%2\ifcengine.dll
-copy %RDF_IFC_ENGINE_LIB%%2\ifcengine.dll ifcengine\lib\x64\%2\ifcengine.dll /Y
-if .%1.==.. goto LibUpdated
-echo Update %1%2\ifcengine.dll
-copy ifcengine\lib\x64\%2\ifcengine.dll %1%2\ifcengine.dll /Y
+echo Update lib and dll
+xcopy %RDF_IFC_ENGINE_LIB%Debug\ifcengine.lib ifcengine\lib\x64\ /F /Y
+xcopy %RDF_IFC_ENGINE_LIB%%2\ifcengine.dll ifcengine\lib\x64\%2\ /F /Y
+echo Copy dll to output
+xcopy ifcengine\lib\x64\%2\ifcengine.dll %1 /F /Y
 :LibUpdated
