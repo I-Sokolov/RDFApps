343048f6c3419317101bfa1ea458edd32d55830c
Igor.Sokolov <I.Sokolov@ya.ru>
2022-12-20 11:36:38 +0400

    UpdateIfcEngine includes platform (x64/win32)

--- ifcviewer/UpdateIfcEngine.bat
+++ ifcviewer/UpdateIfcEngine.bat
@@ -8,8 +8,8 @@ xcopy %RDF_IFC_ENGINE_INCLUDE% IfcEngine\include /F /Y
 echo Check IFC engine binaries
 if .%RDF_IFC_ENGINE_LIB%.==.. goto LibUpdated
 echo Update lib and dll
-xcopy %RDF_IFC_ENGINE_LIB%Debug\ifcengine.lib ifcengine\lib\x64\ /F /Y
-xcopy %RDF_IFC_ENGINE_LIB%%2\ifcengine.dll ifcengine\lib\x64\%2\ /F /Y
+xcopy %RDF_IFC_ENGINE_LIB%Debug\ifcengine.lib ifcengine\lib\%3\ /F /Y
+xcopy %RDF_IFC_ENGINE_LIB%%2\ifcengine.dll ifcengine\lib\%3\%2\ /F /Y
 echo Copy dll to output
-xcopy ifcengine\lib\x64\%2\ifcengine.dll %1 /F /Y
+xcopy ifcengine\lib\%3\%2\ifcengine.dll %1 /F /Y
 :LibUpdated
